eb67a2221a4c3823494ca9e028f590a57401259f
scepbjoern <scep@zhaw.ch>
2019-11-10 14:32:09 +0100

    Initial commit

---
+++ src/main/java/ch/zhaw/iwi/gpi/aacsb/AacsbServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.iwi.gpi.aacsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AacsbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AacsbServiceApplication.class, args);
+	}
+
+}
